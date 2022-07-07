c44d7255c038994deaa211fa288ccac30bd18d43
kayyenl <yenyenlee9@gmail.com>
2022-07-07 19:21:35 +0800

    halfway done with hard i will come back and finish

--- hard.js
+++ hard.js
@@ -15,8 +15,35 @@ function sortLowToHigh(array) {
     let newarray = []
     let ogLength = array.length
     for (let i = 0; i < ogLength; i++) {
+        let smallestnum = array[0]
+        let smallindex = 0
         for (let j = 0; j < array.length; j++) {
-            
+            if (array.length === 1) smallestnum = array[j]
+            else {
+                if (smallestnum > array[j]) {
+                    smallestnum = array[j]
+                    smallindex = j
+                }
+            }
         }
+        newarray.push(smallestnum)
+        array.splice(smallindex, 1)
     }
+    return newarray
+}
+
+// console.log(sortLowToHigh([20,40,10,30,50,10]))
+// console.log(sortLowToHigh([5,10,0,-5]))
+// console.log(sortLowToHigh([3,2,1,0]))
+
+function sortUsingComparator(array) {
+    return array.sort((a, b) => a - b)
+} // a - b is ascending and b - a is descending.
+
+// console.log(sortUsingComparator([20,40,10,30,50,10]))
+// console.log(sortUsingComparator([5,10,0,-5]))
+// console.log(sortUsingComparator([3,2,1,0]))
+
+function sortHighToLow(array) {
+    
 }
