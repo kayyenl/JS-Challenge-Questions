c4c22745c1250644af81bef4eca1ceda1609cbeb
kayyenl <yenyenlee9@gmail.com>
2022-07-07 14:40:24 +0800

    starting on the beginner qns

---
+++ app.js
@@ -0,0 +1,9 @@
+ //BEGINNER CHALLENGES
+
+ function calcRemainder(number, divisor) {
+    return number % divisor
+ }
+ 
+ console.log(calcRemainder(4,2))
+ console.log(calcRemainder(7,8))
+ console.log(calcRemainder(9,8))
